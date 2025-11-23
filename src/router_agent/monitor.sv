54ac3d02be6c6b02bc56405a19bc8e8020f48519
ValdiviaM <hvaldivia@estudiantec.cr>
2025-11-22 23:25:20 -0600

    Add all signals to an item on monitor

--- src/router_agent/monitor.sv
+++ src/router_agent/monitor.sv
@@ -20,29 +20,29 @@ class router_monitor extends uvm_monitor;
 
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
-
+212
         forever begin
             @(vif.clk);
                 if (!vif.reset) begin
                     seq_item item = seq_item::type_id::create("item");
-                    //logica de leer interfaz y pasarlas al item
-                //     input clk, reset,
-                //  input popin, data_out, pndng,
-                //  input pop, data_out_i_in, pndng_i_in
-// rand bit [ADDR_WIDTH-1:0] addr;
-// 	rand bit [DATA_WIDTH-1:0] data;
-// 	rand bit [$clog2(MAX_N_CYCLES)-1:0] cycles_between;
-// 	rand route_mode_e mode;
-	
-// 	// Flags
-// 	rand error_type_e msg_error;
-// 	rand bit broadcast;     // Si el mensaje es broadcast
+                // rand bit [ADDR_WIDTH-1:0] addr;
+                // 	rand bit [DATA_WIDTH-1:0] data;
+                // 	rand bit [$clog2(MAX_N_CYCLES)-1:0] cycles_between;
+                // 	rand route_mode_e mode;
+                    
+                // 	// Flags
+                // 	rand error_type_e msg_error;
+                // 	rand bit broadcast;     // Si el mensaje es broadcast
 
-// 	//Salidas
-// 	bit [DATA_WIDTH-1:0] out_dut;
+                // 	//Salidas
+                // 	bit [DATA_WIDTH-1:0] out_dut;
+                    $display("Inputs: data_out_i_in=%b  pndng_i_in=%b  pop=%b",
+                            vif.data_out_i_in, vif.pndng_i_in, vif.pop);
+
+                    $display("Outputs: data_out=%b  pndng=%b  popin=%b",
+                            vif.data_out, vif.pndng, vif.popin);
 
                     item.out_dut=vif.data_out_i_in
-
                     mon_analysis_port.write(item);
                 `uvm_info("MON", $sformatf("Saw item %s", item.convert2str()), UVM_HIGH)
                 end
